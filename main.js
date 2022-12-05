9d785e07f95d3be85c143895d137336b1980a550
Yo7X <117323614+Yo7X@users.noreply.github.com>
2022-12-04 21:06:33 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Index
 function invite() {
     window.open("https://discord.com/api/oauth2/authorize?client_id=970014291413565520&permissions=2147613712&scope=applications.commands%20bot", '_blank')
 }
@@ -7,7 +8,7 @@ function x() {
 }
 
 function cls() {
-    document.getElementById('Wbuilding').style.top = 1000 + 'px'
+    document.getElementById('Wbuilding').style.top = -100000 + 'px'
 }
 
 
@@ -19,13 +20,15 @@ function general() {
     document.getElementById('sb03').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb04').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb06').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
 
     document.getElementById('generalpage').style.top = 0 + 'px'
 
-    document.getElementById('commandspage').style.top = 1000 + 'px'
-    document.getElementById('privacypage').style.top = 1000 + 'px'
-    document.getElementById('aboutpage').style.top = 1000 + 'px'
-    document.getElementById('patchpage').style.top = 1000 + 'px'
+    document.getElementById('commandspage').style.top = -100000 + 'px'
+    document.getElementById('privacypage').style.top = -100000 + 'px'
+    document.getElementById('aboutpage').style.top = -100000 + 'px'
+    document.getElementById('patchpage').style.top = -100000 + 'px'
+    document.getElementById('morepage').style.top = -100000 + 'px'
 }
 
 function commands() {
@@ -34,14 +37,16 @@ function commands() {
     document.getElementById('sb01').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb03').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb04').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
-    document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)' 
+    document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb06').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
 
     document.getElementById('commandspage').style.top = 0 + 'px'
 
-    document.getElementById('generalpage').style.top = 1000 + 'px'
-    document.getElementById('privacypage').style.top = 1000 + 'px'
-    document.getElementById('aboutpage').style.top = 1000 + 'px'
-    document.getElementById('patchpage').style.top = 1000 + 'px'
+    document.getElementById('generalpage').style.top = -100000 + 'px'
+    document.getElementById('privacypage').style.top = -100000 + 'px'
+    document.getElementById('aboutpage').style.top = -100000 + 'px'
+    document.getElementById('patchpage').style.top = -100000 + 'px'
+    document.getElementById('morepage').style.top = -100000 + 'px'
 }
 
 function privacy() {
@@ -51,13 +56,15 @@ function privacy() {
     document.getElementById('sb02').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb04').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb06').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
 
     document.getElementById('privacypage').style.top = 0 + 'px'
 
-    document.getElementById('generalpage').style.top = 1000 + 'px'
-    document.getElementById('commandspage').style.top = 1000 + 'px'
-    document.getElementById('aboutpage').style.top = 1000 + 'px'
-    document.getElementById('patchpage').style.top = 1000 + 'px'
+    document.getElementById('generalpage').style.top = -100000 + 'px'
+    document.getElementById('commandspage').style.top = -100000 + 'px'
+    document.getElementById('aboutpage').style.top = -100000 + 'px'
+    document.getElementById('patchpage').style.top = -100000 + 'px'
+    document.getElementById('morepage').style.top = -100000 + 'px'
 }
 
 function about() {
@@ -67,13 +74,15 @@ function about() {
     document.getElementById('sb02').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb03').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb06').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
 
     document.getElementById('aboutpage').style.top = 0 + 'px'
 
-    document.getElementById('generalpage').style.top = 1000 + 'px'
-    document.getElementById('commandspage').style.top = 1000 + 'px'
-    document.getElementById('privacypage').style.top = 1000 + 'px'
-    document.getElementById('patchpage').style.top = 1000 + 'px'
+    document.getElementById('generalpage').style.top = -100000 + 'px'
+    document.getElementById('commandspage').style.top = -100000 + 'px'
+    document.getElementById('privacypage').style.top = -100000 + 'px'
+    document.getElementById('patchpage').style.top = -100000 + 'px'
+    document.getElementById('morepage').style.top = -100000 + 'px'
 }
 
 function patch() {
@@ -83,11 +92,31 @@ function patch() {
     document.getElementById('sb02').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb03').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('sb04').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb06').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
 
     document.getElementById('patchpage').style.top = 0 + 'px'
 
-    document.getElementById('generalpage').style.top = 1000 + 'px'
-    document.getElementById('commandspage').style.top = 1000 + 'px'
-    document.getElementById('privacypage').style.top = 1000 + 'px'
-    document.getElementById('aboutpage').style.top = 1000 + 'px'
+    document.getElementById('generalpage').style.top = -100000 + 'px'
+    document.getElementById('commandspage').style.top = -100000 + 'px'
+    document.getElementById('privacypage').style.top = -100000 + 'px'
+    document.getElementById('aboutpage').style.top = -100000 + 'px'
+    document.getElementById('morepage').style.top = -100000 + 'px'
+}
+
+function more() {
+    document.getElementById('sb06').style.backgroundColor = 'rgba(55, 45, 158, 0.911)'
+
+    document.getElementById('sb01').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb02').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb03').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb04').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('sb05').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+
+    document.getElementById('morepage').style.top = 0 + 'px'
+
+    document.getElementById('patchpage').style.top = -100000 + 'px'
+    document.getElementById('generalpage').style.top = -100000 + 'px'
+    document.getElementById('commandspage').style.top = -100000 + 'px'
+    document.getElementById('privacypage').style.top = -100000 + 'px'
+    document.getElementById('aboutpage').style.top = -100000 + 'px'
 }
